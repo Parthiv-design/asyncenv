ddf1810b9bb81715406989205afccacddba1e325
Parthiv-Design <TaskSnap.manage@gmail.com>
2026-07-24 12:52:02 -0600

    Deploy horizontal single-line matrix logging layout engine for version 3.3.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="asyncenv",
-    version="3.3.5",
+    version="3.3.6",
     author="Enterprise Engineering Group",
     description="Cooperative Task Slicing Architecture for Micro-Runtimes.",
     packages=find_packages(),
